736e710a9061dc9b3fa941c3b74ec967d35ce7ad
fairleecomp <leereddy122@gmail.com>
2025-04-15 14:48:30 +0100

    alpha 0.06 - Added metronome bugs - Metronome sound not working, possibly not imported properly - Metronome function loops and starts a new recording every time - Record button initially appears blue

--- Loom/ViewControllers/RecordingViewController.swift
+++ Loom/ViewControllers/RecordingViewController.swift
@@ -4,24 +4,94 @@ import AVFoundation
 class RecordingViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var bpmSlider: UISlider!
+    @IBOutlet weak var bpmLabel: UILabel!
     
     var audioRecorder: AVAudioRecorder?
     var recordedFileURL: URL?
     
+    // Metronome properties
+    var metronomeTimer: Timer?
+    var currentBeat: Int = 0
+    
+    // AVAudioPlayers for metronome sounds
+    var accentSoundPlayer: AVAudioPlayer?
+    var tickSoundPlayer: AVAudioPlayer?
+    
+    var metronomeBPM: Float {
+        return bpmSlider.value
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        recordButton.setTitle("", for: .normal)
+        
+        
+        // Configure BPM slider
+        bpmSlider.minimumValue = 40
+        bpmSlider.maximumValue = 240
+        bpmSlider.value = 120
+        updateBPMLabel()
+        
+        // Load metronome sound files (ensure accent.wav and tick.wav are added to your project bundle)
+        if let accentURL = Bundle.main.url(forResource: "Perc_PracticePad_hi", withExtension: "wav") {
+            accentSoundPlayer = try? AVAudioPlayer(contentsOf: accentURL)
+            accentSoundPlayer?.prepareToPlay()
+        }
+        if let tickURL = Bundle.main.url(forResource: "Perc_PracticePad_lo", withExtension: "wav") {
+            tickSoundPlayer = try? AVAudioPlayer(contentsOf: tickURL)
+            tickSoundPlayer?.prepareToPlay()
+        }
+    }
+    
+    @IBAction func bpmSliderChanged(_ sender: UISlider) {
+        updateBPMLabel()
+    }
+    
+    func updateBPMLabel() {
+        bpmLabel.text = "BPM: \(Int(bpmSlider.value))"
     }
     
     @IBAction func recordButtonTapped(_ sender: UIButton) {
         if audioRecorder == nil {
-            startRecording()
-            recordButton.setTitle("Stop", for: .normal)
+            startMetronomeCountdown()
         } else {
             stopRecording()
-            recordButton.setTitle("Record", for: .normal)
+            stopMetronomeCountdown()
+            stopOverlayImageAnimation()
         }
     }
+    
+    // MARK: - Metronome Methods
+    
+    func startMetronomeCountdown() {
+        currentBeat = 0
+        let interval = Double(60 / metronomeBPM)
+        metronomeTimer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(metronomeTick), userInfo: nil, repeats: true)
+        animateOverlayImage()
+    }
+    
+    @objc func metronomeTick() {
+        currentBeat += 1
+        if currentBeat == 1 {
+            print("Beat 1 (accented)")
+            accentSoundPlayer?.play()
+        } else {
+            print("Beat \(currentBeat)")
+            tickSoundPlayer?.play()
+        }
+        if currentBeat >= 4 {
+            stopMetronomeCountdown()
+            startRecording()
+        }
+    }
+    
+    func stopMetronomeCountdown() {
+        metronomeTimer?.invalidate()
+        metronomeTimer = nil
+        currentBeat = 0
+    }
+    
+    // MARK: - Recording Methods
     
     func startRecording() {
         let session = AVAudioSession.sharedInstance()
@@ -53,20 +123,27 @@ class RecordingViewController: UIViewController {
         audioRecorder?.stop()
         audioRecorder = nil
         print("Recording stopped.")
-        
-        
-            
-            // When stopping the recording:
-            if let url = recordedFileURL {
-                // Assume the recording is assigned to a particular track.
-                // If you know the track number (say track 1 for simplicity):
-                
-                MixerManager.shared.assignRecording(url: url)
-                
-                // Update the state to playing for that track.
-                // (Alternatively, you might update the state from within MixerManager's assignRecording method.)
-                NotificationCenter.default.post(name: NSNotification.Name("MixerManagerUpdated"), object: nil)
-            }
+        if let url = recordedFileURL {
+            MixerManager.shared.assignRecording(url: url)
+            NotificationCenter.default.post(name: NSNotification.Name("MixerManagerUpdated"), object: nil)
         }
     }
-
+    
+    // MARK: - Overlay Animation Methods
+    
+    func animateOverlayImage() {
+        guard let overlayImageView = recordButton.subviews.first(where: { $0 is UIImageView }) as? UIImageView else { return }
+        let pulseAnimation = CABasicAnimation(keyPath: "transform.scale")
+        pulseAnimation.duration = 0.8
+        pulseAnimation.fromValue = 1.0
+        pulseAnimation.toValue = 1.2
+        pulseAnimation.repeatCount = Float.infinity
+        pulseAnimation.autoreverses = true
+        overlayImageView.layer.add(pulseAnimation, forKey: "breathe")
+    }
+    
+    func stopOverlayImageAnimation() {
+        guard let overlayImageView = recordButton.subviews.first(where: { $0 is UIImageView }) as? UIImageView else { return }
+        overlayImageView.layer.removeAnimation(forKey: "breathe")
+    }
+}
